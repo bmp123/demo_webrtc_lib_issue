2a131ed96adf8312a140310c9b2dbdec7568189d
Ivan <404@onlinepbx.ru>
2023-02-02 09:48:56 +0500

    init

---
+++ demo-webrtc/Demo.swift
@@ -0,0 +1,9 @@
+//
+//  Demo.swift
+//  demo-webrtc
+//
+//  Created by ИВАН on 02.02.2023.
+//
+
+
+import WebRTC
---
+++ demo-webrtc/demo_webrtc.h
@@ -0,0 +1,18 @@
+//
+//  demo_webrtc.h
+//  demo-webrtc
+//
+//  Created by ИВАН on 02.02.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for demo_webrtc.
+FOUNDATION_EXPORT double demo_webrtcVersionNumber;
+
+//! Project version string for demo_webrtc.
+FOUNDATION_EXPORT const unsigned char demo_webrtcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <demo_webrtc/PublicHeader.h>
+
+
